fb662715dbf399cf7f86ec358f605bb170da9c54
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-11-12 12:23:32 -0600

    Added Verification Navigator

--- invex/src/navigation/VerificationNavigator.js
+++ invex/src/navigation/VerificationNavigator.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Verification from "../pages/Verification";
 
 const VerificationNavigator = () => {
   return (
     <Switch>
-        <Route exact path="/verification" component/>
-        <Route/>
+        <Route exact path="/verification" component={Verification}/>
+        <Route path="/" component={Verification}/>
     </Switch>
   );
 };
