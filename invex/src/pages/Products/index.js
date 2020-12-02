29d73d2628d44fa13075cc2af9d414dfaf02bc68
Gustavo-Hernandez <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-12-02 16:50:59 -0600

    Completed Name Filter

--- invex/src/pages/Products/index.js
+++ invex/src/pages/Products/index.js
@@ -3,7 +3,9 @@ import {
   Button,
   Divider,
   Grid,
+  InputBase,
   makeStyles,
+  Paper,
 } from "@material-ui/core";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
@@ -13,6 +15,7 @@ import Fade from "@material-ui/core/Fade";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import ProductCard from "./ProductCard";
 import ProductsForm from "./ProductsForm";
+import SearchIcon from "@material-ui/icons/Search";
 import Chip from "@material-ui/core/Chip";
 import { Context as ItemsContext } from "../../context/ItemsContext";
 import { store } from "react-notifications-component";
@@ -22,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
   },
   options: {
-    marginBottom: "15px",
+    marginBottom: "7px",
   },
   products: {
     width: "100%",
@@ -35,11 +38,25 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: 4,
   },
   filters: {
-    height: "100%",
     width: "100%",
     display: "flex",
     flexDirection: "row",
+    flexWrap: "wrap",
     justifyContent: "space-between",
+  },
+  searchbar: {
+    height: "100%",
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    border: "1px solid lightgray",
+  },
+  filter: {
+    marginBottom: 8,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
   },
 }));
 
@@ -52,6 +69,7 @@ const Products = () => {
   const [loading, setLoading] = useState(false);
   const [filtering, setFiltering] = useState(false);
   const [folderFilter, setFolderFilter] = useState("");
+  const [searchFilter, setSearchFilter] = useState("");
   const [filteredProducts, setFilteredProducts] = useState("");
   const [keyForm, setKey] = useState(1);
 
@@ -99,17 +117,61 @@ const Products = () => {
     setShowForm((prev) => !prev);
   };
 
-  const handleFilterButton = (e) => {
-    if (e.target.value === "") {
-      setFiltering(false)
-    }else{
-      setFiltering(true)
-      let filtered =  state.items.filter(item => item.folder === e.target.value);
-      let filteredComponent = filtered.map((product, index) => (
-        <ProductCard key={index} handleDelete={deleteItem} {...product} />
-      ));
-      setFilteredProducts(filteredComponent);
+  const handleSearch = (e) => {
+    let filtered = [];
+    if(e.target.value === "" && folderFilter === ""){
+      setFiltering(false);
+    }else if(e.target.value === "" && folderFilter !== ""){
+      filtered = state.items.filter(
+        (item) => item.folder === folderFilter
+      );
+    } else{
+      if (folderFilter !== "") {
+        filtered = state.items.filter(
+          (item) =>
+            item.folder === folderFilter && item.name.includes(e.target.value)
+        );
+      } else {
+        filtered = state.items.filter((item) =>
+          item.name.includes(e.target.value)
+        );
+        setFiltering(true);
+      }
     }
+    let filteredComponent = filtered.map((product, index) => (
+      <ProductCard key={index} handleDelete={deleteItem} {...product} />
+    ));
+    setFilteredProducts(filteredComponent);
+    setSearchFilter(e.target.value);
+  };
+
+  const handleFilterButton = (e) => {
+    let filtered=[];
+    if (e.target.value === "" &&  searchFilter === "") {
+      setFiltering(false);
+    }else if(e.target.value === "" && searchFilter !== ""){
+      filtered = state.items.filter(
+        (item) =>
+          item.name.includes(e.target.value)
+      );
+    } 
+    else {
+      if (searchFilter !== "") {
+        filtered = state.items.filter(
+          (item) =>
+            item.folder === e.target.value && item.name.includes(searchFilter)
+        );
+      }else{
+        filtered = state.items.filter(
+          (item) => item.folder === e.target.value
+        );
+        setFiltering(true);
+      }  
+    }
+    let filteredComponent = filtered.map((product, index) => (
+      <ProductCard key={index} handleDelete={deleteItem} {...product} />
+    ));
+    setFilteredProducts(filteredComponent);
     setFolderFilter(e.target.value);
   };
 
@@ -134,9 +196,23 @@ const Products = () => {
     <Fade in>
       <div className={classes.root}>
         <Grid container className={classes.options}>
-          <Grid item style={{ width: "100%" }}>
-            <div className={classes.filters}>
+          <Grid className={classes.filters}>
+            <Grid item xs={12} md={3} className={classes.filter}>
+              <Paper component="form" className={classes.searchbar}>
+                <InputBase
+                  className={classes.input}
+                  placeholder="Search Item"
+                  autoCorrect="none"
+                  onChange={handleSearch}
+                  value={searchFilter}
+                />
+                <SearchIcon />
+              </Paper>
+            </Grid>
+            <Grid item xs={5} md={2} className={classes.filter}>
               <Button
+                fullWidth
+                style={{ height: "100%" }}
                 variant="contained"
                 color="secondary"
                 startIcon={<AddCircleIcon />}
@@ -144,7 +220,9 @@ const Products = () => {
               >
                 {showForm ? "Close" : "Add Product"}
               </Button>
-              <FormControl variant="outlined">
+            </Grid>
+            <Grid item xs={5} md={3} className={classes.filter}>
+              <FormControl fullWidth variant="outlined">
                 <InputLabel id="folder-filter">Filter by Folder</InputLabel>
                 <Select
                   labelId="folder-filter"
@@ -152,7 +230,7 @@ const Products = () => {
                   labelWidth={160}
                   onChange={handleFilterButton}
                   value={folderFilter}
-                  style={{width:"160px"}}
+                  style={{ width: "160px" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -160,8 +238,7 @@ const Products = () => {
                   {folderOptions}
                 </Select>
               </FormControl>
-              
-            </div>
+            </Grid>
           </Grid>
         </Grid>
         {showForm && (
@@ -175,7 +252,9 @@ const Products = () => {
           />
         )}
         <Divider />
-        <div className={classes.products}>{filtering ? filteredProducts : products}</div>
+        <div className={classes.products}>
+          {filtering ? filteredProducts : products}
+        </div>
       </div>
     </Fade>
   );
